cacd0080f811600c82f280b306fd0823520491e8
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-25 12:08:06 +0300

    Add buttons

--- script.js
+++ script.js
@@ -40,3 +40,15 @@ container.appendChild(content);
 container.appendChild(paragraph);
 container.appendChild(title3);
 container.appendChild(newDiv);
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
